aed13ead4e603491381c2aff15fb38913d0be03d
Kenneth Tran <kennetht787@gmail.com>
2022-05-17 16:02:01 -0400

    Add solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (
+    typeof num1 != "number" ||
+    typeof num2 != "number" ||
+    num1 < 0 ||
+    num2 < 0
+  ) {
+    return "ERROR";
+  }
 
+  let sum = 0;
+  let lowerBound = num1 > num2 ? num2 : num1;
+  let upperBound = num1 > num2 ? num1 : num2;
+
+  for (let i = lowerBound; i <= upperBound; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
