6a9cea543d5719ec691a8668c3f82d4c953367e5
Kenneth Tran <kennetht787@gmail.com>
2022-05-17 14:21:50 -0400

    Add solution to exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let reversedCharacterArray = [...string].reverse();
+  let reversedString = "";
+  reversedCharacterArray.forEach((char) => {
+    reversedString += char;
+  });
+  return reversedString;
 };
 
 // Do not edit below this line
