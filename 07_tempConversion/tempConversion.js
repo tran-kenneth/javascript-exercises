8e927c886a52536d53fc83978c014edb974c7242
Kenneth Tran <kennetht787@gmail.com>
2022-05-17 22:30:44 -0400

    Add solution to exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function (tempInF) {
+  return Math.round((10 * ((tempInF - 32) * 5)) / 9) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (tempInC) {
+  return Math.round(10 * ((9 / 5) * tempInC + 32)) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
