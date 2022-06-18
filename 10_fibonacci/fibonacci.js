1c17ca89112f0228243711a52d4009352aa602c4
Kenneth Tran <kennetht787@gmail.com>
2022-06-18 13:10:09 -0400

    Add solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (typeof num == String) num = Number(num);
+  if (num < 1) return "OOPS";
 
+  const fibSeq = [1, 1];
+  if (num > 2) {
+    for (let i = 0; i <= num - 3; i++) {
+      fibSeq.push(fibSeq[fibSeq.length - 1] + fibSeq[fibSeq.length - 2]);
+    }
+  }
+
+  return fibSeq[num - 1];
 };
 
 // Do not edit below this line
