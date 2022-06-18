1c62303180d7c507b5461db1299698665c0d31e1
Kenneth Tran <kennetht787@gmail.com>
2022-06-18 12:58:07 -0400

    Add solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const alphaChars = str.match(/[a-z]+/gi);
+  let recombineString = "";
 
+  alphaChars.forEach(
+    (matchedStr) => (recombineString += matchedStr.toLowerCase())
+  );
+
+  for (let i = 0; i < Math.floor(recombineString.length / 2); i++) {
+    if (recombineString[i] != recombineString[recombineString.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
